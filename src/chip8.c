3afbd9ad3ac26ebad308844be242294528834091
ivanhaaro <ivanharoliminana@gmail.com>
2024-09-30 19:36:45 +0200

    boilerplate completed

--- src/chip8.c
+++ src/chip8.c
@@ -4,15 +4,30 @@
 #include <stdbool.h>
 #include <SDL.h>
 
+// SDL container object
 typedef struct {
     SDL_Window *window;
     SDL_Renderer *renderer;
 } sdl_t;
 
+// Emulator configuration object
 typedef struct {
     uint32_t window_width;
     uint32_t window_height;
+    uint32_t bg_color; // Background color RGB
 } config_t;
+
+// Emulator states
+typedef enum {
+    QUIT,
+    RUNNING,
+    PAUSED,
+} emulator_state_t;
+
+//CHIP8 machine 
+typedef struct {
+    emulator_state_t state;
+} chip8_t;
 
 bool init_sdl(sdl_t *sdl, config_t *config) {
 
@@ -43,6 +58,7 @@ bool set_up_config(config_t *config, int argc, char **argv) {
     *config = (config_t) {
         .window_width = 1280,
         .window_height = 640,
+        .bg_color = 0x0000FF00,
     };
 
     //Override defaults from arguments
@@ -59,6 +75,42 @@ void final_cleanup(sdl_t sdl) {
     SDL_Quit();
 }
 
+void clear_screen(const sdl_t sdl, const config_t config) {
+    const uint8_t r = (config.bg_color >> 24) & 0xFF;
+    const uint8_t g = (config.bg_color >> 16) & 0xFF;
+    const uint8_t b = (config.bg_color >>  8) & 0xFF;
+    const uint8_t a = (config.bg_color >>  0) & 0xFF;
+
+    SDL_SetRenderDrawColor(sdl.renderer, r, g, b, a);
+    SDL_RenderClear(sdl.renderer);
+}
+
+void update_screen(const sdl_t sdl) {
+    SDL_RenderPresent(sdl.renderer);
+}
+
+void handle_input(chip8_t *chip8){
+    SDL_Event event;
+
+    while(SDL_PollEvent(&event)) {
+        switch(event.type) {
+        
+            case SDL_QUIT:
+                //Exit window. End program
+                chip8->state = QUIT;
+                return;
+
+            default:
+                break;
+        }
+    }
+}
+
+bool init_chip8(chip8_t *chip8) {
+    chip8->state = RUNNING;
+    return true;
+}
+
 int main(int argc, char **argv) {
 
     // Initialize the configuration of the emulator
@@ -69,13 +121,23 @@ int main(int argc, char **argv) {
     sdl_t sdl = {0};
     if(!init_sdl(&sdl, &config)) exit(EXIT_FAILURE);
 
+    // Initalize CHIP8 machine
+    chip8_t chip8 = {0};
+    if(!init_chip8(&chip8)) exit(EXIT_FAILURE);
+
     // Initial screen clear
-    //SDL_SetRenderDrawColor(sdl.renderer);
-    SDL_RenderClear(sdl.renderer);
+    clear_screen(sdl, config);
 
     // Main emulator loop
-    while(true) {
+    while(chip8.state != QUIT) {
+        // Handle user input
+        handle_input(&chip8);
+        
+        // Delay for approximately 60Hz (16.67ms)
+        SDL_Delay(16);
 
+        // Update window with changes
+        update_screen(sdl);
     }
 
     // Final cleanup
